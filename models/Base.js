0b8a35a66f17ce5ba95ff1903d7faedcc6d9ff63
Adonis <adopi.naj@gmail.com>
2014-02-02 18:23:54 +0100

    Base singleton added

--- models/Base.js
+++ models/Base.js
@@ -1,10 +1,53 @@
+"use strict";
+//dependencies
+var mysql = require('mysql');
+
+//mysql config
+var config = {
+	database: 'webmusic',
+	user: 'adonis',
+	host: 'localhost',
+	password: 'BgMacaquePokemon36'
+}
+
 //Base singleton
-var base = (function() {
+var Base = (function() {
+
+	//function which makes Base object with options 
+	function createSingleton() {
+		var options = config;
+		/** 
+		* set options to the options supplied
+		* or an empty object if none are provided
+		**/
+		options = options || {};
+
+		//mysql object which is used to connect at the mysql server
+		return mysql.createConnection({
+			database: options.database || "database",
+			host: options.host || "localhost",
+			user: options.user || "root",
+			password: options.password || "root"
+		});
+	};
+
 	//instance of the singleton
 	var instance;
 
-	function init() {
-		
-	},
-	
-});
+	//makes singleton and returns itself
+	var _static = {
+			//method to get Base instance
+			getConnection: function() {			
+				if (instance===undefined) {
+					instance = createSingleton();
+				}
+				return instance;
+			}
+	};
+
+	//returns the static object when it is created
+	return _static;
+})();
+
+//export the object base
+module.exports = Base;
--- test/connexion.js
+++ test/connexion.js
@@ -1,12 +1,14 @@
+"use strict";
+//dependencies
 var assert = require("assert")
-
+var mysql = require("mysql");
+//tests
 describe('Base', function(){
   describe('#connexion()', function(){
     it('should get an object which allows to connect at mysql server', function(){
-    		var base = new Base();
-    		var instance = base.getInstance();
-    	};
-    	};
+    		var base = require("../models/Base").getConnection();
+    		assert(base);
     })
   })
+  
 })
