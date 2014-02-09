6edcf389ed19f0fb2d381ffdce7d1602b282eef3
Adonis <adopi.naj@gmail.com>
2014-02-09 11:01:10 +0100

    tests updated

--- test/db.js
+++ test/db.js
@@ -12,8 +12,8 @@ describe('User', function(){
             done(error);
         });
     });
-  describe('#insert()', function(done){
-    it('should create a new user and insert it on the database', function(){
+  describe('#insert()', function(){
+    it('should create a new user and insert it on the database', function(done){
     	var user = User.build({
     		user_firstname: 'Kebab',
     		user_lastname: 'Potatoes',
@@ -27,5 +27,22 @@ describe('User', function(){
     		done();
     	});
     });
+    describe('#delete()', function(){
+    it('should delete the latest user inserted',function(done){
+            User.find({where:{'user_nickname':'ElKebabo'}})
+            .success(function(user){
+                user.destroy()
+                .success(function(){
+                done();
+                })
+                .error(function(error){
+                    done(error);
+                });
+            })
+            .error(function(error){
+                done(error);
+            });
+        });
+    });   
   });
 });
