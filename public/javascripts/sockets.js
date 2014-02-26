c31d0f045adc3e9beb9c897f233d545c4e8da2c1
Adonis <adopi.naj@gmail.com>
2014-02-26 11:46:26 +0100

    fix issue with the id in the url

--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -2,16 +2,17 @@ var socket = io.connect('http://localhost');
 var followButton = $('#button-follow');
 socket.emit('user',{});
 socket.on('followerAdded',function(data) {
-  $(".followers ul").append(data.nickname);
+  $(".followers ul").append("<li>"+data.nickname+"</li>");
 });
 
 followButton.click(function() {
   //get the current profil id
   var path = location.pathname;
-  var id = path.split('/')[2];
+  var id = path.split('/')[3].match('[0-9]*')[0];
+  alert(id);
   //send the id of the current user
   socket.emit('follow',{
     idUser:id,
-    idFollower:1
+    idFollower:4
   });
 });
