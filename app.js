fa2b768371848fa3d2880700a835d4f36bd73fdf
Adonis <adopi.naj@gmail.com>
2014-03-08 09:55:22 +0100

    move handlebars helpers in a separate file

--- app.js
+++ app.js
@@ -7,55 +7,18 @@ var path = require('path');
 var app = express();
 var db = require('./models');
 var io = require('socket.io');
-var cookieParser = express.cookieParser();
-var sessionStore = new express.session.MemoryStore();
+var SessionSockets = require('session.socket.io')
+
 //define the secret string to crypt cookies
 var SITE_SECRET = process.env.SECRET || 
 									require('./config/session').secret() ||
 									'test';
-//handlebarjs
-var hbs = require('express3-handlebars').create({
-	helpers: {
-		img: function(name,alt) { return '<img src="images/'+name+'" alt="'+alt+'">';},
-		styles: function() {
-			//get directory name
-			var fs = require('fs');
-			var files;
-			/* make the array which will be used for css files*/
-			files= fs.readdirSync(__dirname+'/public/stylesheets');
-			//make the string result
-			var result='';			
-			for (var i = files.length - 1; i >= 0; i--) {
-				var file = files[i];
-				var tab = file.split('.');
-				if (tab[tab.length-1]=='css')
-					result+='<link rel="stylesheet" href="/stylesheets/'+file+'">';
-			}
-			return result;
-		},
-		css: function(name) {
-			return '<link rel="stylesheet" href="/stylesheets/'+name+'.css">';
-		},
-		js: function(name) {
-			return '<script src="/javascripts/'+name+'.js" type="text/javascript"></script>';
-		},
-		//specific to this application
-		listing: function(hashmap) {
-			var out = '';
-			for(var key in hashmap) {
-				out += '<p>'+key+'</p>';
-				//tabs
-				for(var i in hashmap[key]) {
-					var tab = hashmap[key][i];
-					out += '<li class="tab-list"><a href="/tab/profil/'+tab.id+'">'+tab.name+'</a></li>';
-				}
-			}
-			return out;	
-		}
-	}
-});
+var cookieParser = express.cookieParser(SITE_SECRET);
+var sessionStore = new connect.middleware.session.MemoryStore();
 
-app.engine('handlebars', hbs.engine);
+//handlebarjs
+
+app.engine('handlebars', require('./config/helpers').engine);
 
 
 
@@ -68,12 +31,14 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-//managing session
-app.use(cookieParser);
-app.use(express.session({
-	secret: SITE_SECRET,
-	store: sessionStore
-}));
+
+//managing session 
+app.configure(function() {
+	app.use(cookieParser);
+	app.use(express.session({
+		store: sessionStore
+	}));
+});
 
 //res global variables
 app.use(function(req, res, next){
@@ -104,8 +69,15 @@ var routes = require('./config/routes')(app);
 var server = http.createServer(app);
 io = io.listen(server);
 
+//define the session socket
+var sessionSockets = new SessionSockets(io,sessionStore,cookieParser);
+
+sessionSockets.getIo = function() {
+	return  io;
+};
+
 //loading sockets config
-var sockets = require('./config/sockets')(io);
+var sockets = require('./config/sockets')(sessionSockets);
 
 //start the express server
 server.listen(app.get('port'), function(){
---
+++ config/hbs.js
@@ -0,0 +1,42 @@
+//configuration file for handlebars 
+
+var hbs = require('express3-handlebars').create({
+  helpers: {
+    img: function(name,alt) { return '<img src="images/'+name+'" alt="'+alt+'">';},
+    styles: function() {
+      //get directory name
+      var fs = require('fs');
+      var files;
+      /* make the array which will be used for css files*/
+      files= fs.readdirSync(__dirname+'/public/stylesheets');
+      //make the string result
+      var result='';      
+      for (var i = files.length - 1; i >= 0; i--) {
+        var file = files[i];
+        var tab = file.split('.');
+        if (tab[tab.length-1]=='css')
+          result+='<link rel="stylesheet" href="/stylesheets/'+file+'">';
+      }
+      return result;
+    },
+    css: function(name) {
+      return '<link rel="stylesheet" href="/stylesheets/'+name+'.css">';
+    },
+    js: function(name) {
+      return '<script src="/javascripts/'+name+'.js" type="text/javascript"></script>';
+    },
+    //specific to this application
+    listing: function(hashmap) {
+      var out = '';
+      for(var key in hashmap) {
+        out += '<p>'+key+'</p>';
+        //tabs
+        for(var i in hashmap[key]) {
+          var tab = hashmap[key][i];
+          out += '<li class="tab-list"><a href="/tab/profil/'+tab.id+'">'+tab.name+'</a></li>';
+        }
+      }
+      return out; 
+    }
+  }
+});
