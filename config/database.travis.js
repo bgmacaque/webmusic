cb5a16227a657dca08ff255f3f845602637723df
Adonis <adopi.naj@gmail.com>
2014-04-02 22:48:29 +0200

    update travis configuration file

--- config/database.travis.js
+++ config/database.travis.js
@@ -2,5 +2,13 @@ module.exports = {
 	database: 'myapp_test',	
 	user: 'travis',	
 	host: '127.0.0.1',	
-	password: null 
+	password: null,
+  options : {
+    dialect: "mysql",
+    host : 'localhost',
+    logging:false,
+    define: { 
+      underscored: true
+    }
+  }
 }
